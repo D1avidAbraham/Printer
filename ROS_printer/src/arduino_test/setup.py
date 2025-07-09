c3888a2b8a83f3cdca914b82ecb0f49d8fae23fc
David Abraham <d1avidabraham@gmail.com>
2025-07-09 12:19:49 -0400

    fixed setup.py

--- ROS_printer/src/arduino_test/setup.py
+++ ROS_printer/src/arduino_test/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "stepper = arduino_test.stepper:main"
+            "stepper = arduino_test.stepper:main",
             "distance = arduino_test.distance:main"
         ],
     },
